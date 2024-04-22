986201158b9c298fbd6f9c0a4c56369de4eee5bc
emillyvsl <emilly.vitoria@sou.ufac.br>
2024-04-22 14:59:29 -0500

    fix:footer adjustment

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ import { playersRouter } from "./src/routes/playeres.routes.js";
 import { scoreRouter } from "./src/routes/score.routes.js";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(errorHandler);
